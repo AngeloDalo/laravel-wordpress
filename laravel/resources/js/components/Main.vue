0b766735fc3fa92cb2b44631764e0fcc9dbf2a8b
AngeloDalo <angelo251158@gmail.com>
2022-03-10 14:16:49 +0100

    random Home

--- laravel/resources/js/components/Main.vue
+++ laravel/resources/js/components/Main.vue
@@ -2,9 +2,9 @@
 <template>
   <div class="container">
     <div class="row row-cols-1 row-cols-md-4 g-4">
-      <div class="col" v-for="(post, index) in posts" :key="index">
+      <div class="col" v-for="(post, index) in cards.posts" :key="index">
         <div class="card">
-          <img :src="'/storage/'+post.image" class="card-img-top" :alt="post.name">
+          <img :src="'/storage/'+post.image" class="card-img-top" :alt="post.title">
           <div class="card-body">
             <h5 class="card-title">{{ post.title }}</h5>
             <p class="card-text">{{ post.content }}</p>
@@ -12,10 +12,10 @@
         </div>
       </div>
     </div>
-    <div class="row mt-2 mb-5 bg-light">
+    <div class="row mt-3 bg-light" v-if="cards.prev_page_url || cards.next_page_url">
       <ul class="list-inline bg-light">
-        <li class="list-inline-item"> <button v-if="prev_page_url" class="btn btn-primary" @click="changePage('prev_page_url')">Prev</button></li>
-        <li class="list-inline-item"> <button v-if="next_page_url" class="btn btn-primary" @click="changePage('next_page_url')">Next</button></li>
+        <li class="list-inline-item"> <button v-if="cards.prev_page_url" class="btn btn-primary" @click="changePage('prev_page_url')">Prev</button></li>
+        <li class="list-inline-item"> <button v-if="cards.next_page_url" class="btn btn-primary" @click="changePage('next_page_url')">Next</button></li>
       </ul>
     </div>
 
@@ -51,21 +51,21 @@
 </template>
 
 <script>
-import Axios from "axios";
-  export default {
-    name: "Main",
-    data() {
-      return {
-        posts: null,
-        next_page_url: null,
-        prev_page_url: null
-      }
-    },
-    created() {
+export default {
+  name: "Main",
+  props: ['cards'],
+    // data() {
+    //   return {
+    //     posts: null,
+    //     next_page_url: null,
+    //     prev_page_url: null
+    //   }
+    // },
+    // created() {
       //passo alla funzione getPost il link per il file json
       //nella funzione il file è url
       //se il file url esiste(json) allora in this.post che prima era vuoto vanno i post
-      this.getPosts('http://127.0.0.1:8000/api/posts');
+      // this.getPosts('http://127.0.0.1:8000/api/posts');
     //     Axios.post('http://127.0.0.1:8000/api/posts', {
     //     orderby: 'title',
     //     order: 'desc'
@@ -76,28 +76,30 @@ import Axios from "axios";
     //   .catch(function (error) {
     //     console.log(error);
     //   });
-    },
-    methods: {
+    // },
+  methods: {
       //in changePage dico se voglio la pagina successiva o quella precedente
       //andrò a questo punto a riempira la variabile in return
       //Se la variabile non esiste perchè siamo arrivati a fine pagina o all'inizio non verrà mostrato il pulsante
-      changePage(vs) {
-        let url = this[vs];
-        if(url) {
-          this.getPosts(url);
-        }
-      },
-      getPosts(url){
-          Axios.get(url).then(
-            (result) => {
-              this.posts = result.data.results.data;
-              this.next_page_url = result.data.results.next_page_url;
-              this.prev_page_url = result.data.results.prev_page_url;
-            });
-      }
-      
+      // changePage(vs) {
+      //   let url = this[vs];
+      //   if(url) {
+      //     this.getPosts(url);
+      //   }
+      // },
+      // getPosts(url){
+      //     Axios.get(url).then(
+      //       (result) => {
+      //         this.posts = result.data.results.data;
+      //         this.next_page_url = result.data.results.next_page_url;
+      //         this.prev_page_url = result.data.results.prev_page_url;
+      //       });
+      // }
+    changePage(vs) {
+      this.$emit('changePage', vs);
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
--- laravel/resources/js/pages/Home.vue
+++ laravel/resources/js/pages/Home.vue
@@ -1,24 +1,59 @@
 <template>
-  <div>
-      HOME
-  </div>
+    <div>
+        <div class="container-fluid">
+            <div class="row">
+            <div class="col text-center m-2">
+                <h1>Home</h1>
+            </div>
+            </div>
+                <Main :cards="cards" @changePage="changePage($event)"></Main>
+            </div>
+    </div>
 </template>
 
 <script>
-//import Axios from "axios";
+import Axios from "axios";
+import Main from '../components/Main.vue';
   export default {
     name: 'Home',
+    components: {
+      Main
+    },
     data() {
       return {
+        cards: {
+          posts: null,
+          next_page_url: null,
+          prev_page_url: null
+        }
       }
     },
     created() {
-      this.getProducts('http://127.0.0.1:8001/api/v1/posts/random');
+      this.getPosts('http://127.0.0.1:8000/api/v1/posts/random');
     },
     methods: {
+      changePage(vs) {
+        let url = this.cards[vs];
+        if(url) {
+          this.getPosts(url);
+        }
+      },
+      getPosts(url){
+          Axios.get(url).then(
+            (result) => {
+              console.log(result);
+              this.cards.posts = result.data.results.data;
+              this.cards.next_page_url = result.data.results.next_page_url;
+              this.cards.prev_page_url = result.data.results.prev_page_url;
+            });
+      }
+      
     }
   }
 </script>
+
+<style lang="scss">
+</style>
 
 <style lang="scss">
 </style>
